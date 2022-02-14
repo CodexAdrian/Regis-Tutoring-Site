97d901869616e943bcf825ca9f423cd307545f33
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2022-02-14 14:46:34 -0500

    Added user api stuff

--- api/user-api.php
+++ api/user-api.php
@@ -1,10 +1,22 @@
 <?php
 
-function getUserProfile(int $userid) {
+const BASE_URI = "https://moodle.regis.org/webservice/rest/server.php";
+
+function getUserProfile(int $userid, String $token) {
     //TODO make $exists equal to whether or not the user exists in the table
     $exists = false;
 
     if($exists) {
-
+        //TODO grab data from database
+    } else {
+        $userProfile1 = file_get_contents(BASE_URI. "?wstoken=$token&moodlewsrestformat=json&field=id&id=$userid");
+        $userProfile2 = file_get_contents(BASE_URI. "?wstoken=$token&moodlewsrestformat=json&values[]=86");
+        $profilePicture;
+        $firstName;
+        $lastName;
+        $userName;
+        $isDeptHead;
+        $picture;
+        $userType;
     }
 }
