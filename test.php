8aadf3e37e43b5997bf96ae83bce9bafb3484814
orbofdyskord <95365309+orbofdyskord@users.noreply.github.com>
2022-02-14 14:58:11 -0500

    Put indents on test

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <html>
-<body>
-<?php
-include "login-api.php";
-$token = getUserToken($_POST['username'], $_POST['password']);
-echo $token . "<br>";
-$id = getUserID($token);
-echo $id;
-?>
-</body>
+    <body>
+        <?php
+            include "login-api.php";
+            $token = getUserToken($_POST['username'], $_POST['password']);
+            echo $token . "<br>";
+            $id = getUserID($token);
+            echo $id;
+        ?>
+    </body>
 </html>
