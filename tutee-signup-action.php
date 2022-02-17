0608f5b17c9f04a6cedb5162e6110dc5c275414e
orbofdyskord <95365309+orbofdyskord@users.noreply.github.com>
2022-02-17 14:59:23 -0500

    Updated tutee-signup-action and tutor-application-action

--- tutee-signup-action.php
+++ tutee-signup-action.php
@@ -4,13 +4,13 @@
 	include "functions.php";
 	
 	# Write form post variables into local variables
-	//All the user's information can be accessed by the session variables
-	$prefDayID = $_POST['prefDayID'];
-	$prefTimeID = $_POST['prefTimeID'];
-	// $senderID = $_GET['userID'] // also need the userID through a get variable in order to run the insert statement. This GET variable will not work right now. 
-	
-	// echo ; 
-	// exit;
+	$eventID = $_POST[''];
+	$tutorID = $_POST[''];
+	$eventDate = $_POST[''];
+	$topicID = $_POST[''];
+	$periodID = $_POST[''];
+	$fileContents = $_POST[''];
+	$filename = random_bytes(20);
 	
 
 	#Connecting to database server
@@ -18,13 +18,12 @@
 		or die("Error: Cannot connect to database server");
 
 		
-	$sql = "INSERT INTO tutorApplications
-		(recipientID, senderID, subjectID, refTeacherID, prefDayID, prefTimeID) 
+	$sql = "
+		INSERT INTO calendarEvents
+		(eventID, tutorID, studentID, eventDate, topicID, periodID, extraMaterial) 
 		VALUES 
-		($recipientID, $senderID, $subjectID, $refTeacherID, $prefDayID, $prefTimeID)";		
-
-	// will not execute, need to get the senderID first
-	//echo $sql . "<br>";
+		($eventID, $tutorID, $_SESSION['userID'], $eventDate, $topicID, $periodID, $fileName)
+	";	
 	
 	// insert the row into the table
 	$rs = mysqli_query($dbc, $sql);
@@ -35,6 +34,58 @@
 	else {
 		echo "Record Insertion Failed!";	
 	}
+
+	#Emailing the tutor that a new tutee has signed up
+	$sql1 = "
+		SELECT * 
+		FROM users 
+		WHERE userID = $recipientID
+	";
+	$rs1 = mysqli_query($dbc, $sql1);
+	$row1 = mysqli_fetch_array($rs1);
+	$recipientFirstName = $row1['firstName'];
+	$recipientLastName = $row1['lastName'];
+	$recipientFullName = $recipientFirstName . " " . $recipientLastName;
+	$recipientUsername = $row1['userName'];
+
+	$userFirstInitial = substr($_SESSION['firstName'], 0, 0);
+
+	$fullDate = getdate();
+	$date = $fullDate['mon'] . "/" . $fullDate['mday'] . "/" . $fullDate['year'];
+	$time = $fullDate['hours'] . ":" . $fullDate['minutes'] . ":" . $fullDate['seconds'];
+	$fileContents = 
+		"From: " . $_SESSION['fullname'] . "at $date $time" . "\r\n" .
+		"To: $recipientFullName, signing up for tutoring." . "\r\n" . 
+		$tutorReason
+	;
+
+	$myfile = fopen("tutee-signups/$filename.txt", "w") or die("Unable to generate file!");		//Double check that this file declaration works
+	fwrite($myfile, $fileContents);
+	
+
+	$to = $recipientUsername . "@regis.org";		//Can be replaced by getEmail function eventually
+	$subject = "New Tutor Application from: " . $_SESSION['fullName'];
+	$message =
+		"Dear $recipientFullName: \r\n" . 
+		$_SESSION['fullName'] . " has signed up for tutoring in ___, referencing " . $refTeacherFullName . "." . "\r\n" .
+		"Here is their reasoning: $tutor"
+	;	//2/17: Need to pull the subject
+	$header = 
+		"From: " . $_SESSION['fullName'] . " <" . $_SESSION['username'] . "@regis.org>" . "\r\n" . 
+		"CC: " . $refTeacherFullName . "@regis.org"
+	;
+	
+	if (mail($to,$subject,$message,$headers)) {
+		echo "Your application has successfully been submitted.";
+	}
+	else{
+		echo "Something went wrong.";
+	}
+
+	fclose($myfile);
+?>
+
+<a href="homepage.php">Go back to the Home Page</a>
 	
 ?>
 
