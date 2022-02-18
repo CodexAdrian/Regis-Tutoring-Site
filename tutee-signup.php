6097cd3bf7582f19ac8570f32e9efab5ad5607f2
orbofdyskord <95365309+orbofdyskord@users.noreply.github.com>
2022-02-17 21:28:16 -0500

    Updated tutee-signup.php

--- tutee-signup.php
+++ tutee-signup.php
@@ -1,8 +1,6 @@
 <?php
     include "session.php";
-include "session.php";
-if (isset($_SESSION['userID'])) {
-
+    if (isset($_SESSION['userID'])) {
         include "auth.php";
         include "nav.php";
         include "functions.php";
