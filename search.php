b0403ec18f0da4e2ac30bb5a66937ed7b001f451
jjpie13 <95635706+jjpie13@users.noreply.github.com>
2022-02-18 03:54:52 -0500

    update search23

--- search.php
+++ search.php
@@ -13,29 +13,18 @@ if ($_SESSION['userID']) {
     <br>
 
 
-<!--
-<div class = "m-5">
-<p class="text-white text-2xl">Tutoring Info</p>
-    <br>
-    <br>
-</div>
--->
 
 <form action = "search.php" method = "post">
-    <input name = "searchString" size = "20" placeholder = "Search the database1">
+    <input name = "searchString" size = "20" placeholder = "Search the database">
     <button type="submit" name="search" value="searchDatabase">
 </form>
-<<<<<<< Updated upstream
-</div>
-=======
 
 
->>>>>>> Stashed changes
 <?php
     if(isset($_POST['searchString'])) {
         $searchString = $_POST['searchString'];
         
-        echo "<p class=\"text-slate-400 text-2xl mb-2 font-bold\">Data Results</p>";
+        echo "<p class=\"text-white text-2xl mb-2 font-bold\">Data Results</p>";
         echo "<br>";
 
         #Searching teachers
