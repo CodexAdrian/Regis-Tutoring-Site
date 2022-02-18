eb5c63c8bd4586873aaeedf87c8b73511f85e03c
jjpie13 <95635706+jjpie13@users.noreply.github.com>
2022-02-17 22:58:57 -0500

    update tutee signup

--- tutee-signup.php
+++ tutee-signup.php
@@ -44,7 +44,7 @@ if (isset($_SESSION['userID'])) {
                 $subjectName = $row['subjectName'];
                 $timeBlockName = $row['timeBlockName'];
 
-                echo "<option class ='bg-gray-700' value = '$tutorID'>$firstName $lastName, $topicName, $timeBlockName </option>";
+                echo "<option class ='bg-gray-700' value='$tutorID'>$firstName $lastName, $topicName, $timeBlockName </option>";
 
                 //Echo out all the tutors and their respespective subjects in a dropdown, storing the selected tutor's ID to 
             }
@@ -73,12 +73,46 @@ if (isset($_SESSION['userID'])) {
             $endTime = $row['endTime'];
             $timeBlockName = $row['timeBlockName'];
 
-            echo "<option class ='bg-gray-700' value = '$periodID'>$startTime , $endTime ,  $timeBlockName </option>";
+            echo "<option class ='bg-gray-700' value ='$periodID'>$startTime-$endTime,  $timeBlockName </option>";
 
         }
 
         ?>
         </select>
+
+
+        <select class ='bg-gray-700' name="eventDate">
+        <?php
+
+        $sql3 = "
+            SELECT * 
+                FROM dateDesc 
+                WHERE dateDesc.schoolDate > DATE(SYSDATE())
+                ORDER BY schoolDate;  
+            ";
+
+
+        $rs = mysqli_query($dbc, $sql3);     
+        //2/15/22 The person signing up will need to select the tutor, the day, the topic and the timeperiod at a minimum
+        //2/15/22 Will also have to pass in the person's ID and the path to extra material if they want
+
+        //for now, each tutor is linked to only one topic. So by passing the tutorID, we know what topic they are tutoring. (Need a many to many to do more topics down the road)
+        $index = 0;
+        while ($row = mysqli_fetch_array($rs)) {
+            $index++;
+            $schoolDate = $row['schoolDate'];
+            $letterDay = $row['letterDay'];
+
+            echo "<option class ='bg-gray-700' value ='$schoolDate'>$schoolDate, $letterDay</option>";
+            
+            if ($index >= 5){
+                break;
+            }
+        }
+
+        ?>
+        </select>
+    
     
 
 </form> 
